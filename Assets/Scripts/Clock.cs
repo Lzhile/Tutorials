a5b05edcbbbc6b351d139e8246407f6b53784424
Lzhile <zhile.li@foxmail.com>
2020-12-31 09:46:39 +0800

    commit unity project

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    const float hoursToDegress = -30f, minutesToDegress = -6f, secondsToDegress = -6f;
+
+    [SerializeField]
+    Transform hoursPivot = default, minutesPivot = default, secondsPivot = default;
+
+    void Awake()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegress*(float)time.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegress*(float)time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegress*(float)time.TotalSeconds);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegress*(float)time.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegress*(float)time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegress*(float)time.TotalSeconds);
+    }
+}
---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab = default;
+
+    void Awake()
+    {
+        Transform point = default;
+        int length = 10;
+        for (int i = 0; i < length; i++)
+        {
+            point = Instantiate(pointPrefab);
+            point.localPosition = Vector3.right * i;
+            point.localScale = Vector3.one / 5f;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
