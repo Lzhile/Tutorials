46be7838c0ce241ba49bba3d9a65f35635a6e21b
Lzhile <46445197+Lzhile@users.noreply.github.com>
2021-01-04 00:01:38 +0800

    Finish building-a-graph

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -9,20 +9,24 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
+    Transform[] points;
+
     void Awake()
     {
         Transform point = default;
         float step = 2f / resolution;
         var scale = Vector3.one * step;
         var position = Vector3.zero;
-        for (int i = 0; i < resolution; i++)
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++)
         {
             point = Instantiate(pointPrefab);
             position.x = (i+0.5f)*step-1f;
-            position.y = position.x * position.x * position.x;
+            //position.y = position.x * position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
+            points[i] = point;
         }
     }
 
@@ -35,6 +39,13 @@ public class Graph : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time / 5));
+            point.localPosition = position;
+        }
     }
 }
